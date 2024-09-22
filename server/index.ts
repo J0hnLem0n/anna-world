efb298741dfe917ed8f0373004bb7ba0f646392c
ENikolaev <nikolaevee@gmail.com>
2024-09-22 21:52:34 +0300

    add apload file url

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import { jwt } from "@elysiajs/jwt";
 import { cors } from "@elysiajs/cors";
 import { eq } from "drizzle-orm";
 import * as schema from "./schema";
 import { db } from "./db";
 import { staticPlugin } from "@elysiajs/static";
+import crypto from "crypto";
 
 const port = 8080;
 const protocol = "http";
@@ -45,9 +46,25 @@ app
       message: "Invalid credentials",
     };
   })
+  .post(
+    "/item",
+    async ({ body, redirect }) => {
+      const fileName = body.file.name;
+      if (!fileName) throw new Error("Must upload a file.");
+      await Bun.write(
+        `public/${crypto.randomUUID()}.${fileName.split(".").pop()}`,
+        body.file
+      );
+      // return { res: "/" };
+      return redirect("http://localhost:3000/upload.html");
+    },
+    {
+      body: t.Object({
+        file: t.File(),
+      }),
+    }
+  )
   .get("/items", async () => {
-    // console.dir(Bun.file("public/table.png"));
-
     return (await db.select().from(schema.items)).map((i) => ({
       ...i,
       image: baseUrl + i.image,
